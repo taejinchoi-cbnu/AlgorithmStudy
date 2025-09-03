d35a21e930178d46d513aca212951b99a46dbdcb
taejinchoi-cbnu <xowls000@chungbuk.ac.kr>
2025-09-03 11:02:23 +0900

    [week1] day3 최적화

--- 0x03/BOJ3273.js
+++ 0x03/BOJ3273.js
@@ -4,41 +4,36 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-let inputs = [];
+let inputs;
 let lineCnt = 0;
 let n = 0;
 let x = 0;
-let res = 0;
 
 rl.on('line', (line) => {
   if (lineCnt === 0) {
     n = Number(line);
-    lineCnt++;
+      lineCnt++;
   } else if (lineCnt === 1) {
-    inputs.push(
-      ...line
-        .split(' ')
-        .map(Number)
-        .sort((a, b) => a - b)
-    );
-    lineCnt++;
+      inputs = line.split(' ').map(Number).sort((a, b) => a - b);
+      lineCnt++;
   } else {
-    x = Number(line);
-    rl.close();
+      x = Number(line);
+      rl.close();
   }
 }).on('close', () => {
-  let left = 0;
-  let right = n - 1;
-  while (left < right) {
-    let sum = inputs[left] + inputs[right];
-    if (sum === x) {
-      res++;
-      right--;
-    } else if (sum > x) {
-      right--;
-    } else if (sum < x) {
-      left++;
+    let res = 0;
+    let left = 0;
+    let right = n - 1;
+    while (left < right) {
+        let sum = inputs[left] + inputs[right];
+        if (sum === x) {
+            res++;
+            right--;
+        } else if (sum > x) {
+            right--;
+        } else {
+            left++;
+        }
     }
-  }
-  console.log(res);
+    console.log(res);
 });
